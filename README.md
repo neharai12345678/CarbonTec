f6126e960ebd1389281691ccad50f6ff5afc3add
Neha Rai <nehrai@microsoft.com>
2022-09-23 13:59:23 +0530

    icon updated

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # CarbonTec
-Clone the project
+1.1Clone the project
 install all below packages :
-pip install win10toast_click
-pip install matplotlib 
-pip install pandas
-Click on run
+1.pip install win10toast_click
+2.pip install matplotlib 
+3.pip install pandas
+4.Click on run
+
+To generate the .exe run below command on VS terminal
+ 1.python -m pip install auto-py-to-exe then wait for installation
+ 2.python -m auto_py_to_exe
+
+ For Django
+ 1.python -m django startproject Project
--- main.py
+++ main.py
@@ -2,19 +2,18 @@ from email import header
 from email.mime import image
 import time
 from turtle import forward
-import winsound
 from win10toast_click import ToastNotifier
 from CarbonFootPrintGraph import *
 from CarbonFootPrintWebPage import *
 from SustainabilityPage import *
 from Repository import *
 if __name__ == '__main__':
-	data=ConnectRegistery();
+	# data=ConnectRegistery();
 	toaster = ToastNotifier()
 	toaster.show_toast(
+		title="CarbonTec"
 		"**Your Carbon foot print!!",
-		"A carbon footprint is the total greenhouse gas (GHG) emissions caused by an individual, event, organization, service, place or product, expressed as carbon dioxide equivalent (CO2e).",
-		icon_path=".\images\co2Print.ico",
+		icon_path=".\images\CarbonTecLogo.ico",
 		duration=12,
 		threaded=False,
 		callback_on_click=openSettings
